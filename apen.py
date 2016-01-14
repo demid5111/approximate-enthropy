f1dada45ee4d9454db8f1c0fd3dbb3c1da1d8495
demid5111 <monadv@yandex.ru>
2016-01-14 07:55:18 +0300

    moved all logic to the ApEn class. TODO: finish logic with calculation of deviation and preapre for multiprocessing of several files in threads

--- apen.py
+++ apen.py
@@ -2,69 +2,82 @@ from math import log
 
 __author__ = 'demidovs'
 
-def read_series(fileName):
-	res = []
-	with open(fileName,"r") as f:
-		for val in f.readlines():
-			res.append(int(val.strip()))
-	return res
+class ApEn:
+	def __init__(self,m,r):
+		self.N = -1
+		self.m = m
+		self.r = r
+		self.u_list = []
+		self.x_list = []
+		self.c_list = []
 
-def create_vectors(N, m,u_list):
-	res_vectors = []
-	for i in range(N-m+1):
-		res_vectors.append(u_list[i:i+m])
-	return res_vectors
+	def read_series(self,fileName):
+		self.u_list = []
+		with open(fileName,"r") as f:
+			for val in f.readlines():
+				self.u_list.append(int(val.strip()))
+		assert self.u_list, "File is either missed or corrupted"
+		assert len(self.u_list)>=300, "Sample length is too small. Need more than 300"
 
+	def create_vectors(self,m):
+		self.x_list = []
+		for i in range(self.N-m+1):
+			self.x_list.append(self.u_list[i:i+m])
 
-def calculate_distance(x1, x2):
-	assert len(x1) == len(x2), "Vectors should be of equal sizes: " + str(x1) + " : " + str(x2)
-	res = []
-	for i in range(len(x1)):
-		res.append(abs(x1[i] - x2[i]))
-	return max(res)
-
-
-def calculate_c(N,m,r,x_list):
-	c_vector = []
-	for i in range(0,N-m+1):
-		similar_vectors = 0
-		for j in range(0,N-m+1):
-			res = calculate_distance(x_list[i],x_list[j])
-			if (res < r):
-				similar_vectors += 1
-		c_vector.append(similar_vectors/(N-m+1))
-	return c_vector
+	def calculate_distance(self,x1, x2):
+		assert len(x1) == len(x2), "Vectors should be of equal sizes: " + str(x1) + " : " + str(x2)
+		res = []
+		for i in range(len(x1)):
+			res.append(abs(x1[i] - x2[i]))
+		return max(res)
 
 
-def calculate_final(N, m, c_list):
-	return sum([log(c_list[i]) for i in range(N-m+1)])*((N-m+1)**(-1))
+	def calculate_c(self,m):
+		self.c_list = []
+		for i in range(0,self.N-m+1):
+			similar_vectors = 0
+			for j in range(0,self.N-m+1):
+				res = self.calculate_distance(self.x_list[i],self.x_list[j])
+				if (res < r):
+					similar_vectors += 1
+			self.c_list.append(similar_vectors/(self.N-m+1))
 
-def calculate_apen(N,m,u_list):
-	# 3. Form a sequence of vectors so that
-	# x[i] = [u[i],u[i+1],...,u[i+m-1]]
-	# i ~ 0:N-m because indexes start from 0
-	x_list = create_vectors(N=N,m=m,u_list=u_list)
 
-	# 4. Construct the C(i,m) - portion of vectors "similar" to i-th
-	# similarity - d[x(j),x(i)], where d = max(a)|u(a)-u*(a)|
-	# this is just the respective values subtraction
-	c_list =  calculate_c(N=N,m=m,r=r,x_list=x_list)
+	def _final(self,m):
+		return sum([log(self.c_list[i]) for i in range(self.N-m+1)])\
+		       *((self.N-m+1)**(-1))
 
-	res1 = calculate_final(N=N,m=m,c_list=c_list)
-	return res1
+	def calculate_final(self,m):
+		self.N = len(self.u_list)
+		# 3. Form a sequence of vectors so that
+		# x[i] = [u[i],u[i+1],...,u[i+m-1]]
+		# i ~ 0:N-m because indexes start from 0
+		self.create_vectors(m=m)
+
+		# 4. Construct the C(i,m) - portion of vectors "similar" to i-th
+		# similarity - d[x(j),x(i)], where d = max(a)|u(a)-u*(a)|
+		# this is just the respective values subtraction
+		self.calculate_c(m=m)
+
+		res1 = self._final(m=m)
+		return res1
+
+	def calculate_apen(self,m):
+		return self.calculate_final(m) - self.calculate_final(m+1)
 
 if __name__ == "__main__":
-	# 1. Read values: u(1), u(2),...,u(N)
-	u_list = read_series("data/sample.dat")
-	assert u_list, "File is either missed or corrupted"
-	assert len(u_list)>=300, "Sample length is too small. Need more than 300"
-
 	# 2. Fix m and r
 	# TODO: compute r later as the value from the deviation
 	m = 2 # m is 2 in our case
 	r = 500 # r is now random, not sure which are real values
-	N = len(u_list)
 
-	res1 = calculate_apen(N=N,m=m,u_list=u_list)
-	res2 = calculate_apen(N=N,m=m+1,u_list=u_list)
-	print(res1-res2)
+	apEn = ApEn(m=2,r=500)
+	# 1. Read values: u(1), u(2),...,u(N)
+	apEn.read_series("data/sample.dat")
+
+	apEn.calculate_apen(m=m)
+	apEn.calculate_apen(m=m+1)
+
+
+	res1 = apEn.calculate_apen(m=m)
+	print(res1)
