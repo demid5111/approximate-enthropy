c262531254a1567acc675b2fff29b13427ecce92
Alexander Demidovskij <alexander.demidovskij@intel.com>
2016-11-20 12:03:21 +0300

    added specific paths

--- launch_win.bat
+++ launch_win.bat
@@ -1,3 +1,3 @@
-git reset --hard origin/master
-git pull --rebase
-python gui_version.py
+"C:\Program Files\Git\bin\git.exe" reset --hard origin/master
+"C:\Program Files\Git\bin\git.exe" pull --rebase
+"C:\Program Files\Anaconda3\python" gui_version.py
