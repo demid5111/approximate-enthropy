5561720f468a2c828d96643e480719b966045849
Alexander Demidovskij <alexander.demidovskij@intel.com>
2017-09-27 20:40:02 +0300

    Refactored ui to accept new target. No valid reporting, no valid structure yet

--- cordim.py
+++ cordim.py
@@ -14,7 +14,7 @@ class CorDim(ApEn):
         for i in seq:
             for j in seq:
                 res.append(CorDim.calc_heviside(radius, ApEn.calculate_distance(i, j)))
-        coef = 1/(len(seq)**2) if len(seq) > 0 else 1
+        coef = 1 / (len(seq) ** 2) if len(seq) > 0 else 1
         return coef * sum(res)
 
     @staticmethod
@@ -22,6 +22,22 @@ class CorDim(ApEn):
         cor_val = log(cor_func_res) if cor_func_res != 0 else 1
         radius_val = log(radius) if radius != 0 and radius != 1 else 1
         return cor_val / radius_val
+
+    @staticmethod
+    def calculate_cor_dim(file_name, dimension, radius):
+        # 1. read the file
+        seq_list = ApEn.read_series(file_name, use_threshold=False, threshold_value=0)
+        portrait = CorDim.slice_intervals(dimension, seq_list)
+        res = CorDim.calc_cor_func(portrait, radius)
+        attractor = CorDim.calc_attractor(res, radius)
+        return {
+            'result': [attractor],
+            'r': [radius,],
+            'n': dimension,
+            'dimension': dimension,
+            'step_size': 0,
+            'average_rr': [0,]
+        }
 
 
 if __name__ == '__main__':
--- gui_version.py
+++ gui_version.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
                              QHBoxLayout, QCheckBox, QBoxLayout, QVBoxLayout, QMessageBox, QMainWindow, QTabWidget)
 
 from apen import ApEn, make_report
+from cordim import CorDim
 from sampen import SampEn
 from supporting import CalculationType
 
@@ -13,28 +14,54 @@ class ApEnWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.fileName = ".memory"
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
-        mLabel = QLabel('m')
-        self.mEdit = QLineEdit("2")
+    def init_ui(self):
+        layout = QVBoxLayout(self)
 
-        rLabel = QLabel("r")
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Entropy")
+        self.tabs.addTab(self.tab2, "CorDim")
+
+        # Create first tab
+        enth_grid = self.config_enthropy_tab()
+        cor_dim_grid = self.config_cordim_tab()
+        self.tab1.setLayout(enth_grid)
+        self.tab2.setLayout(cor_dim_grid)
+
+        # Add tabs to widget
+        layout.addWidget(self.tabs)
+        self.setLayout(layout)
+
+    def config_filechooser_group(self):
+        fileNamesOpen = QPushButton("Open files", self)
+        fileNamesOpen.clicked.connect(self.show_file_chooser)
+
+        fileNamesClean = QPushButton("Clean files", self)
+        fileNamesClean.clicked.connect(self.clean_file_names)
+
+        file_chooser_group = QGridLayout()
+        fileNamesLabel = QLabel("Files to analyze")
+        self.fileNamesEdit = QTextEdit()
+
+        file_buttons_group = QVBoxLayout()
+        file_buttons_group.addWidget(fileNamesOpen)
+        file_buttons_group.addWidget(fileNamesClean)
+
+        file_chooser_group.addWidget(fileNamesLabel, 0, 0)
+        file_chooser_group.addWidget(self.fileNamesEdit, 0, 1)
+        file_chooser_group.addLayout(file_buttons_group, 0, 2)
+
+        return file_chooser_group
+
+    def config_r_enth_group(self):
         number_group = QVBoxLayout()  # Number group
-
-        cb = QCheckBox('Use threshold', self)
-        cb.setChecked(True)
-        cb.clicked.connect(self.toggle_threshold_checkbox)
-        self.rThreshold = QLineEdit("300")
-        self.is_threshold_used = True
-
-        window_cb = QCheckBox('Use windows', self)
-        window_cb.setChecked(True)
-        window_cb.clicked.connect(self.toggle_window_checkbox)
-        self.window_size_edit = QLineEdit("100")
-        self.window_step_edit = QLineEdit("10")
-        self.is_windows_enabled = True
-
         self.calculateR = CalculationType.CONST
         self.rConst = QRadioButton("r=0.2*SDNN")
         self.rConst.setChecked(True)
@@ -55,74 +82,82 @@ class ApEnWidget(QWidget):
         self.rComplex.clicked.connect(self.setComplexR)
         number_group.addWidget(self.rComplex)
 
-        fileNamesLabel = QLabel("Files to analyze")
-        self.fileNamesEdit = QTextEdit()
-        fileNamesOpen = QPushButton("Open files", self)
-        fileNamesOpen.clicked.connect(self.showFileChooser)
+        return number_group
 
-        fileNamesClean = QPushButton("Clean files", self)
-        fileNamesClean.clicked.connect(self.clean_file_names)
+    def config_enthropy_tab(self):
+        mLabel = QLabel('m')
+        self.mEdit = QLineEdit("2")
 
-        apEnCalculate = QPushButton("Calculate ApEn", self)
-        apEnCalculate.clicked.connect(self.calculate_apen)
+        rLabel = QLabel("r")
 
-        self.layout = QVBoxLayout(self)
+        cb = QCheckBox('Use threshold', self)
+        cb.setChecked(True)
+        cb.clicked.connect(self.toggle_threshold_checkbox)
+        self.rThreshold = QLineEdit("300")
+        self.is_threshold_used = True
 
-        # Initialize tab screen
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        # self.tab2 = QWidget()
-        self.tabs.resize(300, 200)
+        window_cb = QCheckBox('Use windows', self)
+        window_cb.setChecked(True)
+        window_cb.clicked.connect(self.toggle_window_checkbox)
+        self.window_size_edit = QLineEdit("100")
+        self.window_step_edit = QLineEdit("10")
+        self.is_windows_enabled = True
 
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Entropy")
+        number_group = self.config_r_enth_group()
 
-        # Create first tab
+        file_chooser_group = self.config_filechooser_group()
 
-        grid = QGridLayout()
-        self.tab1.layout = grid
-        #
-        grid.addWidget(mLabel, 0, 0)
-        grid.addWidget(self.mEdit, 0, 1)
-
-        grid.addWidget(cb, 1, 0)
-        grid.addWidget(self.rThreshold, 1, 1)
-
-        grid.addWidget(window_cb, 2, 0)
-        grid.addWidget(self.window_size_edit, 2, 1)
-        grid.addWidget(self.window_step_edit, 3, 1)
-
-        grid.addWidget(rLabel, 4, 0)
-        grid.addLayout(number_group, 4, 1)
-
-        grid.addWidget(fileNamesLabel, 5, 0)
-        grid.addWidget(self.fileNamesEdit, 5, 1)
-
-        file_buttons_group = QVBoxLayout()
-        file_buttons_group.addWidget(fileNamesOpen)
-        file_buttons_group.addWidget(fileNamesClean)
-
-        grid.addLayout(file_buttons_group, 5, 2)
-
-        grid.addWidget(apEnCalculate, 6, 1, 3, 1)
-
-        self.tab1.setLayout(self.tab1.layout)
+        apen_calculate = QPushButton("Calculate ApEn", self)
+        apen_calculate.clicked.connect(self.calculate_apen)
 
         samp_en_calculate = QPushButton("Calculate SampEn", self)
         samp_en_calculate.clicked.connect(self.calculate_sampen)
 
+        grid = QGridLayout()
+        grid.addWidget(mLabel, 0, 0)
+        grid.addWidget(self.mEdit, 0, 1)
+        grid.addWidget(cb, 1, 0)
+        grid.addWidget(self.rThreshold, 1, 1)
+        grid.addWidget(window_cb, 2, 0)
+        grid.addWidget(self.window_size_edit, 2, 1)
+        grid.addWidget(self.window_step_edit, 3, 1)
+        grid.addWidget(rLabel, 4, 0)
+        grid.addLayout(number_group, 4, 1)
+        grid.addLayout(file_chooser_group, 5, 0, 1, 3)  # spanning it over 3 columns, keeping one row
+        grid.addWidget(apen_calculate, 6, 1, 3, 1)
         grid.addWidget(samp_en_calculate, 9, 1, 3, 1)
-        # self.tab2.setLayout(self.tab2.layout)
 
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
+        return grid
+
+    def config_cordim_tab(self):
+        cor_dim_dimension_label = QLabel('Dimension')
+        self.cor_dim_dimension = QLineEdit("2")
+
+        cor_dim_radius_label = QLabel("radius")
+        self.cor_dim_radius = QLineEdit("0.99")
+
+        file_chooser_group = self.config_filechooser_group()
+
+        cor_dim_calculate = QPushButton("Calculate CorDim", self)
+        cor_dim_calculate.clicked.connect(self.calculate_cor_dim)
+
+        grid = QGridLayout()
+        grid.addWidget(cor_dim_dimension_label, 0, 0)
+        grid.addWidget(self.cor_dim_dimension, 0, 1)
+        grid.addWidget(cor_dim_radius_label, 1, 0)
+        grid.addWidget(self.cor_dim_radius, 1, 1)
+        grid.addWidget(QLabel(''), 2, 0, 3, 3)
+        grid.addLayout(file_chooser_group, 5, 0, 1, 3)  # spanning it over 3 columns, keeping one row
+        grid.addWidget(QLabel(''), 8, 0, 1, 3)
+        grid.addWidget(cor_dim_calculate, 10, 1, 3, 1)
+
+        return grid
 
     def clean_file_names(self):
         self.fileNamesEdit.clear()
 
     def calculate_sampen(self):
-        files_list, threshold_value, dev_coef_value, window_size, step_size = self.get_parameters()
+        files_list, threshold_value, dev_coef_value, window_size, step_size = self.get_entropy_parameters()
         res_dic = {}
         tmp = SampEn()
         for file_name in files_list:
@@ -138,14 +173,12 @@ class ApEnWidget(QWidget):
                 res_dic[file_name] = res
             except ValueError:
                 res_dic[file_name] = {'error': "Error! For file {}".format(file_name)}
-        dialog = QMessageBox(self)
-        dialog.setWindowModality(False)
-        dialog.setText("SampEn calculated for: \n {}".format("".join(["- {}, \n".format(i) for i in res_dic.keys()])))
-        dialog.show()
+
+        self.show_message('SampEn', res_dic)
         make_report(res_dic=res_dic, is_ap_en=False)
 
     def calculate_apen(self):
-        files_list, threshold_value, dev_coef_value, window_size, step_size = self.get_parameters()
+        files_list, threshold_value, dev_coef_value, window_size, step_size = self.get_entropy_parameters()
         res_dic = {}
         tmp = ApEn()
         for file_name in files_list:
@@ -161,13 +194,32 @@ class ApEnWidget(QWidget):
                 res_dic[file_name] = res
             except ValueError:
                 res_dic[file_name] = {'error': "Error! For file {}".format(file_name)}
-        dialog = QMessageBox(self)
-        dialog.setWindowModality(False)
-        dialog.setText("ApEn calculated for: \n {}".format("".join(["- {}, \n".format(i) for i in res_dic.keys()])))
-        dialog.show()
+        self.show_message('ApEn', res_dic)
         make_report(res_dic=res_dic, is_ap_en=True)
 
-    def showFileChooser(self):
+    def calculate_cor_dim(self):
+        files_list, dimension, radius = self.get_cor_dim_parameters()
+        res_dic = {}
+        tmp = CorDim()
+        for file_name in files_list:
+            try:
+                res = tmp.calculate_cor_dim(file_name=file_name,
+                                            dimension=dimension,
+                                            radius=radius)
+                res_dic[file_name] = res
+            except ValueError:
+                res_dic[file_name] = {'error': "Error! For file {}".format(file_name)}
+        self.show_message('CorDim', res_dic)
+        make_report(res_dic=res_dic, is_ap_en=True)
+
+    def show_message(self, source, res_dic):
+        dialog = QMessageBox(self)
+        dialog.setWindowModality(False)
+        dialog.setText(
+            "{} calculated for: \n {}".format(source, "".join(["- {}, \n".format(i) for i in res_dic.keys()])))
+        dialog.show()
+
+    def show_file_chooser(self):
         path = ""
         try:
             with open(self.fileName, "r") as f:
@@ -179,9 +231,9 @@ class ApEnWidget(QWidget):
         print(fname)
         for name in fname[0]:
             self.fileNamesEdit.append(name)
-        self.memorizeLastPath()
+        self.memorize_last_path()
 
-    def memorizeLastPath(self):
+    def memorize_last_path(self):
         if not self.fileNamesEdit:
             return
         path = os.path.dirname(self.fileNamesEdit.toPlainText().split('\n')[0])
@@ -209,7 +261,7 @@ class ApEnWidget(QWidget):
         self.calculateR = CalculationType.DEV
         self.rDevCoef.setEnabled(True)
 
-    def get_parameters(self):
+    def get_entropy_parameters(self):
         files_list = self.fileNamesEdit.toPlainText().split('\n')
         threshold_value = int(self.rThreshold.text()) if self.is_threshold_used else -1
         dev_coef_value = float(self.rDevCoef.text()) if self.calculateR == CalculationType.DEV else -1
@@ -217,13 +269,19 @@ class ApEnWidget(QWidget):
         step_size = int(self.window_step_edit.text()) if self.is_windows_enabled else 0
         return files_list, threshold_value, dev_coef_value, window_size, step_size
 
+    def get_cor_dim_parameters(self):
+        files_list = self.fileNamesEdit.toPlainText().split('\n')
+        dimension = int(self.cor_dim_dimension.text())
+        radius = float(self.cor_dim_radius.text())
+        return files_list, dimension, radius
+
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = 'HeartAlgo-Analyzer'
-        self.left = 0
-        self.top = 0
+        self.left = 400
+        self.top = 200
         self.width = 300
         self.height = 200
         self.setWindowTitle(self.title)
