28773113bba1cc4f67660c4954b8680d9ade6e29
Alexander Demidovskij <alexander.demidovskij@intel.com>
2017-09-30 14:19:26 +0300

    Moved logic of filechooser to a new custom widget

--- src/gui/widgets/ap_en_widget.py
+++ src/gui/widgets/ap_en_widget.py
@@ -1,8 +1,8 @@
 import os
 
 import time
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QPushButton, QGridLayout, QLabel, QTextEdit, QCheckBox, \
-    QLineEdit, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QPushButton, QGridLayout, QLabel, QCheckBox, \
+    QLineEdit, QMessageBox
 
 from src.core.report import CorDimReport, SampEnReport, ApEnReport, ReportManager
 from src.gui.widgets.cor_dim_widget import CorDimWidget
@@ -11,11 +11,14 @@ from src.core.apen import ApEn
 from src.core.cordim import CorDim
 from src.core.sampen import SampEn
 from src.gui.widgets.entropy_widget import EntropyWidget
+from src.gui.widgets.file_chooser_widget import FileChooserWidget
+
 
 class ApEnWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.fileName = ".memory"
+        self.files_selected = False # represent if FileChooserWidget has any files selected
         self.init_ui()
 
     def init_ui(self):
@@ -24,7 +27,6 @@ class ApEnWidget(QWidget):
         # Initialize tab screen
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
-        self.tab2 = QWidget()
         self.tabs.resize(500, 600)
 
         # Add tabs
@@ -38,54 +40,6 @@ class ApEnWidget(QWidget):
         # Add tabs to widget
         layout.addWidget(self.tabs)
         self.setLayout(layout)
-
-    def config_filechooser_group(self):
-        fileNamesOpen = QPushButton("Open files", self)
-        fileNamesOpen.clicked.connect(self.show_file_chooser)
-
-        fileNamesClean = QPushButton("Clean files", self)
-        fileNamesClean.clicked.connect(self.clean_file_names)
-
-        file_chooser_group = QGridLayout()
-        fileNamesLabel = QLabel("Files to analyze")
-        self.fileNamesEdit = QTextEdit(
-                '/Users/demidovs/Documents/Projects/approximate-enthropy/data/ApEn_amolituda_2.txt')
-
-        file_buttons_group = QVBoxLayout()
-        file_buttons_group.addWidget(fileNamesOpen)
-        file_buttons_group.addWidget(fileNamesClean)
-
-        file_chooser_group.addWidget(fileNamesLabel, 0, 0)
-        file_chooser_group.addWidget(self.fileNamesEdit, 0, 1)
-        file_chooser_group.addLayout(file_buttons_group, 0, 2)
-
-        return file_chooser_group
-
-    def clean_file_names(self):
-        self.fileNamesEdit.clear()
-        self.check_run_button_state()
-
-    def show_file_chooser(self):
-        path = ""
-        try:
-            with open(self.fileName, "r") as f:
-                path = f.readline().strip()
-        except FileNotFoundError:
-            pass
-        fname = QFileDialog.getOpenFileNames(self, 'Open file', path, "DAT (*.dat, *.txt)")
-        self.fileNamesEdit.setText("")
-        print(fname)
-        for name in fname[0]:
-            self.fileNamesEdit.append(name)
-        self.memorize_last_path()
-        self.check_run_button_state()
-
-    def memorize_last_path(self):
-        if not self.fileNamesEdit:
-            return
-        path = os.path.dirname(self.fileNamesEdit.toPlainText().split('\n')[0])
-        with open(self.fileName, "w") as f:
-            f.write(path + '/')
 
     def config_entropy_tab(self):
         mLabel = QLabel('m')
@@ -109,8 +63,6 @@ class ApEnWidget(QWidget):
         self.is_use_cor_dim_cb.setChecked(self.is_calc_cor_dim)
         self.is_use_cor_dim_cb.clicked.connect(self.toggle_calc_cor_dim_cb)
 
-        self.cor_dim_widget = CorDimWidget(self)
-
         self.run_calculate = QPushButton("Run calculation", self)
         self.run_calculate.clicked.connect(self.calculate)
 
@@ -123,19 +75,30 @@ class ApEnWidget(QWidget):
         grid.addWidget(self.is_use_ent_cb, 3, 0)
         grid.addWidget(self.ent_widget, 4, 1)
         grid.addWidget(self.is_use_cor_dim_cb, 5, 0)
+
+        self.cor_dim_widget = CorDimWidget(self)
         grid.addWidget(self.cor_dim_widget, 6, 1)
+
         grid.addWidget(self.run_calculate, 10, 0, 3, 3)
 
-        file_chooser_group = self.config_filechooser_group()
-        grid.addLayout(file_chooser_group, 7, 0, 1, 3)
+        self.file_chooser_widget = FileChooserWidget(self, self.fileName)
+        self.file_chooser_widget.new_files_chosen.connect(self.on_new_files_chosen)
+        self.file_chooser_widget.erased_files.connect(self.on_erased_files)
+        grid.addWidget(self.file_chooser_widget, 7, 0, 1, 3)
 
         return grid
+
+    def on_new_files_chosen(self):
+        self.files_selected = True
+
+    def on_erased_files(self):
+        self.files_selected = False
 
     def calculate(self):
         is_ent_enabled = self.is_use_ent_cb
         is_cord_dim_enabled = self.is_use_cor_dim_cb
 
-        files_list = self.get_file_names()
+        files_list = self.file_chooser_widget.get_file_names()
         dimension = int(self.mEdit.text())
         window_size = self.get_window_size()
         step_size = self.get_step_size()
@@ -232,11 +195,8 @@ class ApEnWidget(QWidget):
         self.window_size_edit.setEnabled(self.is_windows_enabled)
         self.window_step_edit.setEnabled(self.is_windows_enabled)
 
-    def get_file_names(self):
-        return [x for x in self.fileNamesEdit.toPlainText().split('\n') if x]
-
     def check_run_button_state(self):
-        self.run_calculate.setEnabled((self.is_calc_ent or self.is_calc_cor_dim) and len(self.get_file_names()) > 0)
+        self.run_calculate.setEnabled((self.is_calc_ent or self.is_calc_cor_dim) and self.files_selected)
 
     def toggle_calc_cor_dim_cb(self):
         self.is_calc_cor_dim = not self.is_calc_cor_dim
---
+++ src/gui/widgets/file_chooser_widget.py
@@ -0,0 +1,64 @@
+import os
+
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QTextEdit, QLabel, QVBoxLayout, QFileDialog
+
+
+class FileChooserWidget(QWidget):
+    new_files_chosen = pyqtSignal()
+    erased_files = pyqtSignal()
+    def __init__(self, parent, file_name):
+        super(QWidget, self).__init__(parent)
+        self.file_name = file_name
+        self.init_ui()
+
+    def init_ui(self):
+        fileNamesOpen = QPushButton("Open files", self)
+        fileNamesOpen.clicked.connect(self.show_file_chooser)
+
+        fileNamesClean = QPushButton("Clean files", self)
+        fileNamesClean.clicked.connect(self.clean_file_names)
+
+        file_chooser_group = QGridLayout()
+        fileNamesLabel = QLabel("Files to analyze")
+        self.file_names_edit = QTextEdit(
+                '/Users/demidovs/Documents/Projects/approximate-enthropy/data/ApEn_amolituda_2.txt')
+
+        file_buttons_group = QVBoxLayout()
+        file_buttons_group.addWidget(fileNamesOpen)
+        file_buttons_group.addWidget(fileNamesClean)
+
+        file_chooser_group.addWidget(fileNamesLabel, 0, 0)
+        file_chooser_group.addWidget(self.file_names_edit, 0, 1)
+        file_chooser_group.addLayout(file_buttons_group, 0, 2)
+
+        self.setLayout(file_chooser_group)
+
+    def clean_file_names(self):
+        self.file_names_edit.clear()
+        self.erased_files.emit()
+
+    def show_file_chooser(self):
+        path = ""
+        try:
+            with open(self.file_name, "r") as f:
+                path = f.readline().strip()
+        except FileNotFoundError:
+            pass
+        fname = QFileDialog.getOpenFileNames(self, 'Open file', path, "DAT (*.dat, *.txt)")
+        self.file_names_edit.setText("")
+        print(fname)
+        for name in fname[0]:
+            self.file_names_edit.append(name)
+        self.memorize_last_path()
+        self.new_files_chosen.emit()
+
+    def memorize_last_path(self):
+        if not self.file_names_edit:
+            return
+        path = os.path.dirname(self.file_names_edit.toPlainText().split('\n')[0])
+        with open(self.file_name, "w") as f:
+            f.write(path + '/')
+
+    def get_file_names(self):
+        return [x for x in self.file_names_edit.toPlainText().split('\n') if x]
