aeef1d5a28470d1493ca45390c8123b1605c76f9
Anastastasya Bakhchina <nastya18-90@mail.ru>
2017-10-03 20:55:18 +0300

    fixed bat file

--- launch_win.bat
+++ launch_win.bat
@@ -1,3 +1,3 @@
 "C:\Program Files\Git\bin\git.exe" reset --hard origin/master
 "C:\Program Files\Git\bin\git.exe" pull --rebase
-"C:\Program Files\Anaconda3\python" gui_version.py
+"C:\Program Files\Anaconda3\python.exe" gui_version.py
